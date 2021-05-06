666e3ad66c57630c1be3ad6c0e97f33538a25d98
Youngsin_2 <donp123@naver.com>
2021-05-06 19:06:21 +0900

    performance improvement

--- src/components/card/card.jsx
+++ src/components/card/card.jsx
@@ -1,9 +1,9 @@
-import React from 'react';
+import React, { memo } from 'react';
 import styles from './card.module.css';
 
-const Card = ({ card }) => {
-    const DEFAULT_IMAGE = '/images/default_logo.png';
+const DEFAULT_IMAGE = '/images/default_logo.png';
 
+const Card = memo(({ card }) => {
     const {
         name,
         company,
@@ -13,7 +13,6 @@ const Card = ({ card }) => {
         theme,
         fileURL
     } = card;
-
     const url = fileURL || DEFAULT_IMAGE;
 
     return (
@@ -28,7 +27,7 @@ const Card = ({ card }) => {
             </div>
         </li>
     );
-};
+});
 
 function getStyles(theme) {
     switch (theme) {
